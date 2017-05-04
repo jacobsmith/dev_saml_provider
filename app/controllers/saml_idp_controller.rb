24f484c2750f3acef12a9746d05831c1fceecc99
Jacob Smith <jacob.wesley.smith@gmail.com>
2017-05-04 16:25:12 -0400

    don't include inresponseto on straight responses

--- app/controllers/saml_idp_controller.rb
+++ app/controllers/saml_idp_controller.rb
@@ -122,6 +122,74 @@ class SamlIdpController < SamlIdp::IdpController
     # render idp-initiated view
   end
 
+  module ::SamlIdp
+    class ResponseBuilder
+      def build
+        builder = Builder::XmlMarkup.new
+        builder.tag! "samlp:Response",
+        ID: response_id_string,
+        Version: "2.0",
+        IssueInstant: now_iso,
+        Destination: saml_acs_url,
+        Consent: Saml::XML::Namespaces::Consents::UNSPECIFIED,
+        "xmlns:samlp" => Saml::XML::Namespaces::PROTOCOL do |response|
+          response.Issuer issuer_uri, xmlns: Saml::XML::Namespaces::ASSERTION
+          response.tag! "samlp:Status" do |status|
+            status.tag! "samlp:StatusCode", Value: Saml::XML::Namespaces::Statuses::SUCCESS
+          end
+          response << assertion_and_signature
+        end
+      end
+    end
+
+    class AssertionBuilder
+      # This redefines #raw to be exactly the same as usual, but without a "InResponseTo" parameter
+      def raw
+        builder = Builder::XmlMarkup.new
+        builder.Assertion xmlns: Saml::XML::Namespaces::ASSERTION,
+        ID: reference_string,
+        IssueInstant: now_iso,
+        Version: "2.0" do |assertion|
+          assertion.Issuer issuer_uri
+          sign assertion
+          assertion.Subject do |subject|
+            subject.NameID name_id, Format: name_id_format[:name]
+            subject.SubjectConfirmation Method: Saml::XML::Namespaces::Methods::BEARER do |confirmation|
+              confirmation.SubjectConfirmationData "",
+              NotOnOrAfter: not_on_or_after_subject,
+              Recipient: saml_acs_url
+            end
+          end
+          assertion.Conditions NotBefore: not_before, NotOnOrAfter: not_on_or_after_condition do |conditions|
+            conditions.AudienceRestriction do |restriction|
+              restriction.Audience audience_uri
+            end
+          end
+          if asserted_attributes
+            assertion.AttributeStatement do |attr_statement|
+              asserted_attributes.each do |friendly_name, attrs|
+                attrs = (attrs || {}).with_indifferent_access
+                attr_statement.Attribute Name: attrs[:name] || friendly_name,
+                NameFormat: attrs[:name_format] || Saml::XML::Namespaces::Formats::Attr::URI,
+                FriendlyName: friendly_name.to_s do |attr|
+                  values = get_values_for friendly_name, attrs[:getter]
+                  values.each do |val|
+                    attr.AttributeValue val.to_s
+                  end
+                end
+              end
+            end
+          end
+          assertion.AuthnStatement AuthnInstant: now_iso, SessionIndex: reference_string do |statement|
+            statement.AuthnContext do |context|
+              context.AuthnContextClassRef authn_context_classref
+            end
+          end
+        end
+      end
+    end
+  end
+
   def encode_authn_response
     principal = User.new(email: params[:email], password: params[:password], params: params)
 
@@ -145,17 +213,14 @@ class SamlIdpController < SamlIdp::IdpController
       audience_uri,
       saml_request_id,
       saml_acs_url,
-      (opts[:algorithm] || algorithm || default_algorithm),
+      default_algorithm, # Sha256
       my_authn_context_classref,
       expiry,
       encryption_opts
     ).build
 
-    # This is the world's crappiest way to make sure that the "InResponseTo" parameter is not included.
-    # Crappy, but it works. =P
-    response = Base64.decode64(response)
-    response.gsub!("InResponseTo=\"\"", "")
-    response = Base64.encode64(response)
+    REXML::Document.new(Base64.decode64(response)).write($stdout, 2) # write to stdout for easier logging
+    puts # get the next rails logger on a new line
 
     render "idp-initiated-response", locals: { saml_acs_url: saml_acs_url, responseParams: { SAMLResponse: response } }
   end
